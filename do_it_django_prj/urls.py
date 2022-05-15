80a4cec1c57a7f56e15a8239c41e8510f26867bb
Bohyun0320 <etoile0320@naver.com>
2022-05-15 21:56:14 +0900

    css와 js 적용

--- do_it_django_prj/urls.py
+++ do_it_django_prj/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 import userInfo
 
 urlpatterns = [
+    path('userInfo/', include('userInfo.urls')),
     path('admin/', admin.site.urls),
 ]
--- userInfo/views.py
+++ userInfo/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
 
 # Create your views here.
+
+def index(request):
+    return render(request, 'userInfo/index.html')
+    # template = loader.get_template('index.html')
+    # return HttpResponse(template.render())
+
+def login(request):
+    return render(
+        request,
+        'userInfo/login.html'
+    )
+
+def join(request):
+    return render(
+        request,
+        'userInfo/join.html'
+    )
