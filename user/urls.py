4c7ee5130c08ac313fa7ef417ec3d5388392df19
wootakjeon <50413487+wootakjeon@users.noreply.github.com>
2022-05-29 05:13:30 +0900

    게시판 기능 추가

--- user/urls.py
+++ user/urls.py
@@ -1,10 +1,18 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
+    path('summernote/', include('django_summernote.urls')),
     path('', views.index, name='index'),
     path('login/', views.login, name='login'),
     path('join/', views.join, name='join'),
     path('logout/', views.logout, name='logout'),
     path('board/', views.board, name='board'),
+    path('board/board_write/', views.board_write, name='board_write'),
+    path('board/board_detail/<int:boardid>/', views.board_detail, name='board_detail'),
+    path('board/board_detail/<int:boardid>/delete/', views.board_delete, name='board_delete'),
+    path('board/board_detail/<int:boardid>/modify/', views.board_modify, name='board_modify'),
+    path('new_comment/<int:boardid>', views.new_comment, name='new_comment'),
+    path('update_comment/<int:boardid>/<int:commentid>', views.update_comment, name='update_comment'),
+    path('delete_comment/<int:boardid>/<int:commentid>', views.delete_comment, name='delete_comment'),
 ]
